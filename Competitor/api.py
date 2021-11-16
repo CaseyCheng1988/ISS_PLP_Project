35b98efe0da9a4335ae6fdec62cea4d7add8900e
TicTacToe <e0507952iss@gmail.com>
2021-11-16 17:31:50 +0800

    After addding get_item_name and Competitor.py

--- Competitor/api.py
+++ Competitor/api.py
@@ -1,33 +1,37 @@
+import os
+import spacy
 from TitleNER.ner import get_entities
-from Shopee.api import product_search, get_review_text
-# from Absa.rule_based_adsa import Rule_Based_ADSA
+from Shopee.api import product_search, get_review_text, get_item_name
 
-
-def get_competitors(title, prod_limit=50, rev_limit=50):
+def get_competitors(title, prod_limit=50, rev_limit=None):
     entities = get_entities(title)
+    print("Detected named entities: ", entities)
 
     ## information to be included in search list
     search_list = [
-        'TYPE', 'DEMOGRAPHIC',
+        'TYPE', 'DEMOGRAPHIC'
     ]
     search_list = [entities[k] for k in search_list]
     search_list = [e for ls in search_list for e in ls]
 
     ## construct Shopee search string here
-    search_string = ' '.join(search_list)
+    search_string = " ".join(search_list)
+    search_string = search_string.split(' ')
+    search_string = "_".join(search_string)
+    print("Searching products similar to: ", search_string, ' ......')
 
     competitors = product_search(search_string, limit=prod_limit)
-
     competitors = [{
         'itemid' :  cp_itemid,
         'shopid' :  cp_shopid,
-        'reviews' : get_review_text(cp_itemid, cp_shopid, limit=rev_limit)
-    } for cp_itemid, cp_shopid in competitors]  # retrieve title too?
+        'title' : get_item_name(cp_itemid, cp_shopid)
+    } for cp_itemid, cp_shopid in competitors]
 
     return competitors
 
 
 if __name__ == '__main__':
     title = 'Fjallraven Kanken Classic Backpack - Red Series'
-    competitors = get_competitors(title, rev_limit=50)
+    competitors = get_competitors(title, prod_limit=20)
     print(competitors)
+
